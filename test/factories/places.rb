9e0849dbd1db892c76ffa85f01d4f348a7d56a4b
Francis Pineda <kikopineda@gmail.com>
2019-07-18 08:23:44 +0800

    :tada: Create Place resource

---
+++ test/factories/places.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :place do
+    name { "MyString" }
+    description { "MyText" }
+    address { "MyString" }
+    mobile { "MyString" }
+    landline { "MyString" }
+    email { "MyString" }
+  end
+end
