81f37a8b37cbbf8d37f23f56f7501185954d8b01
Francis Pineda <sic-f@users.noreply.github.com>
2019-07-20 17:01:20 +0800

    Home page index page (#6) [Finishes #167363405]
    * :white_check_mark: Update Pages system test to include index page testing
    
    * :sparkles: Update Place factory to return more meaningful values
    
    * :bento: Make container that houses side nav and main view to be a container only when in full HD
    
    * :bento: Do not use maximum width on the side navigation
    
    * :construction: Add PagesController#index
    
    * :bento: Create home page index view to display all places in the website
    
    * :bento: Separate hero and navbar partials into their own
    
    * :bento: Occupy the main view by the contents
    
    * :bento: Create a CSS element to take care of centering elements in a wide area view (e.g. main view)
    
    * :bento: Apply .main-view on Pages home page
    
    * :bento: Update _no_places_yet.haml to adjust notice width

--- test/factories/places.rb
+++ test/factories/places.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :place do
-    name { "MyString" }
-    description { "MyText" }
-    address { "MyString" }
-    mobile { "MyString" }
-    landline { "MyString" }
-    email { "MyString" }
+    name { 'Sabang Beach' }
+    description { 'Surf beach.' }
+    address { 'Baler, Aurora' }
+    mobile { 'n/a' }
+    landline { 'n/a' }
+    email { 'sabang@example.com' }
   end
 end
--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,13 +1,13 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class PagesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test 'visiting the index' do
     visit '/'
 
     assert_selector 'h1', text: 'Pagikan'
   end
 
-  test 'navigation' do
+  test 'navigation bar' do
     visit '/'
 
     assert_selector 'nav.navbar', text: 'Home'
@@ -17,5 +17,19 @@ class PagesTest < ApplicationSystemTestCase
     visit '/'
 
     assert_selector 'aside.menu', text: 'ACCOUNT'
+  end
+
+  test 'index page has no places' do
+    visit '/'
+
+    assert_selector '.notification', text: 'There are no places yet.'
+  end
+
+  test 'index page has places' do
+    create :place
+
+    visit '/'
+
+    assert_selector '.title', text: 'Sabang Beach'
   end
 end
