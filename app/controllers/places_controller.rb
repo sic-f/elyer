ac90ae43ccf813cfd21c8d3c831f670bcacfd30d
Francis Pineda <kikopineda@gmail.com>
2019-07-20 21:20:25 +0800

    :construction::sparkles: Add #update in PlacesController

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,13 +9,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    place = Place.new(place_params)
+    @place = Place.new(place_params)
 
-    if place.save
+    if @place.save
       flash[:success] = 'Successfully submitted place!'
-      redirect_to place
+      redirect_to @place
     else
-      flash[:danger] = 'Unable to submit place!'
+      flash[:danger] = 'Unable to submit place.'
       render :new
     end
   end
@@ -24,6 +24,19 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
 
     render :new
+  end
+
+  def update
+    @place = Place.find(params[:id])
+
+    if @place.update(place_params)
+      flash[:success] = 'Update success!'
+
+      redirect_to @place
+    else
+      flash[:danger] = 'Unable to update place.'
+      render :new
+    end
   end
 
   private
