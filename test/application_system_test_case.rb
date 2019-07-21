10d3fd0016c8fb0b8ec248e2f58c85eef7b39359
Francis Pineda <kikopineda@gmail.com>
2019-07-21 13:50:47 +0800

    :wrench: Add deletion of attachments after running system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,13 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  def remove_uploaded_files
+    FileUtils.rm_rf("#{Rails.root}/storage_test")
+  end
+
+  def after_teardown
+    super
+    remove_uploaded_files
+  end
 end
