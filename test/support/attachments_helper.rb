30c4e310afd1914d808e48a28e3448261dd35331
Francis Pineda <kikopineda@gmail.com>
2019-07-21 17:53:01 +0800

    :wrench: Fix argument spacing in AttachmentsHelper

--- test/support/attachments_helper.rb
+++ test/support/attachments_helper.rb
@@ -1,6 +1,6 @@
 module AttachmentsHelper
   # For System Tests
-  def image_upload_file(image_path='public/apple-touch-icon.png')
+  def image_upload_file(image_path = 'public/apple-touch-icon.png')
     Rails.root.join image_path
   end
 
