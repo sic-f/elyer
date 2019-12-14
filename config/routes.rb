2f460e94dfa4c97d56b37ac8adad14d8383f7964
Francis Pineda <kikopineda@gmail.com>
2019-12-14 23:38:51 +0800

    :art: Add route for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'clearance/sessions#new', as: 'sign_in'
 
   # Authentication
   resources :users, only: [:create] do
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
              controller: 'clearance/passwords',
              only: %i[create edit update]
   end
+
+  resource :session, controller: 'clearance/sessions', only: [:create]
 
   resources :places, only: %i[new create show edit update destroy]
 end
