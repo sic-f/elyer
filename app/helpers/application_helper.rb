9ee950043cf3815ff0f964742e8dd9f2fd2b121b
Francis Pineda <kikopineda@gmail.com>
2019-12-15 17:52:37 +0800

    :wrench: Write helper to use PlacePolicy in the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def has_main_photo?(resource, &block)
     capture &block if resource.main_photo.attached?
   end
+
+  def authorized_to(operation, resource)
+    signed_in? && policy(resource).send(operation)
+  end
 end
