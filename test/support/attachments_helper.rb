e3e39656e0255784013ac793befe90978d1a5ab6
Francis Pineda <kikopineda@gmail.com>
2019-12-15 13:50:32 +0800

    :bento: Add another image for ActiveStorage testing

--- test/support/attachments_helper.rb
+++ test/support/attachments_helper.rb
@@ -4,6 +4,10 @@ module AttachmentsHelper
     Rails.root.join image_path
   end
 
+  def another_image_upload_file(image_path = 'test/support/smile.jpg')
+    Rails.root.join image_path
+  end
+
   # For attachments not coming from a web request (local files or downloaded files only)
   def local_file(image_path = 'test/support/apple.jpg')
     { io: File.open(Rails.root.join(image_path)), filename: 'test_image.jpg' }
