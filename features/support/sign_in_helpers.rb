e9e286ca18f19f416ea45ccf75aadad5970f51d3
Francis Pineda <kikopineda@gmail.com>
2019-12-16 00:50:30 +0800

    :truck: Add sign in and attachment helpers to Cucumber

---
+++ features/support/sign_in_helpers.rb
@@ -0,0 +1,11 @@
+module SignInHelpers
+  # For System Tests
+  def sign_in
+    @user = create :user
+    visit root_path as: @user
+  end
+
+  def sign_in_as(user)
+    visit root_path as: user
+  end
+end
