fed96809ac419e0503708175484257cd251ca045
Francis Pineda <kikopineda@gmail.com>
2019-12-16 00:50:56 +0800

    :white_check_mark: Add Cucumber specs for Place operations

---
+++ features/places.feature
@@ -0,0 +1,45 @@
+Feature: Places
+  As a user
+  In order to contribute to the site
+  I want to be able to perform operations on places
+
+  Scenario: Navigate to place submission page
+    Given I am signed in
+    And I click on "Add" link
+    Then I should be on the place submission page
+
+  Scenario: Unaccessible place submission page if not signed in
+    Given I am on the home page
+    Then I should not see the "Add" link to submit a place
+
+  Scenario: Successfully submit a place
+    Given I am signed in
+    And I click on "Add" link
+    When I fill in the required fields to submit a place
+    And I click on "Submit" button
+    Then I should be on the place page
+
+  Scenario: Submit a place with invalid fields
+    Given I am signed in
+    And I click on "Add" link
+    When I fill in the required fields with blank values
+    And I click on "Submit" button
+    Then I should still be on the page submission page with error messages
+
+  Scenario: Update a place with valid fields
+    Given I am signed in and I try to edit a place that has been submitted
+    When I fill in the name field for that place
+    And I click on "Submit" button
+    Then I should be on the place page with an update success message
+
+  Scenario: Update a place with invalid fields
+    Given I am signed in and I try to edit a place that has been submitted
+    When I leave the name and address fields blank for that place
+    And I click on "Submit" button
+    Then I should still be on the place edit page with error messages for name and address
+
+  @javascript
+  Scenario: Deleting a place
+    Given I am signed in and I go to a place's page
+    When I click on the delete "trash can" button
+    Then I should be redirected to the home page with a successful delete message
---
+++ features/step_definitions/place_steps.rb
@@ -0,0 +1,103 @@
+require_relative '../support/attachments_helper'
+require_relative '../support/sign_in_helpers'
+
+include AttachmentsHelper
+include SignInHelpers
+
+Then(/^I should be on the place submission page$/) do
+  assert_selector 'form#new_place', count: 1
+end
+
+Then(/^I should not see the "Add" link to submit a place$/) do
+  assert_no_selector '.navbar-item.new-place-link', count: 1
+end
+
+# Create
+# valid fields
+Then(/^I fill in the required fields to submit a place$/) do
+  fill_in 'place[name]', with: 'Sabang Beach'
+  fill_in 'place[description]', with: 'Surf beach'
+  fill_in 'place[address]', with: 'Baler, Aurora'
+  fill_in 'place[mobile]', with: '09954636990'
+  fill_in 'place[landline]', with: '(02) 952-5463'
+  fill_in 'place[email]', with: 'sabang@example.com'
+  attach_file 'place[main_photo]', image_upload_file, visible: false
+  attach_file 'place[images][]', image_upload_file, visible: false
+end
+
+Then(/^I should be on the place page$/) do
+  assert_selector 'div.notification.is-success', text: 'Successfully submitted place!'
+  assert_selector 'h1.title', text: 'Sabang Beach'
+end
+
+# invalid fields
+When(/^I fill in the required fields with blank values$/) do
+  fill_in 'place[name]', with: ''
+  fill_in 'place[description]', with: 'Surf beach'
+  fill_in 'place[address]', with: ''
+  fill_in 'place[mobile]', with: '09954636990'
+  fill_in 'place[landline]', with: '(02) 952-5463'
+  fill_in 'place[email]', with: 'sabang@example.com'
+  attach_file 'place[main_photo]', image_upload_file, visible: false
+  attach_file 'place[images][]', image_upload_file, visible: false
+end
+
+Then(/^I should still be on the page submission page with error messages$/) do
+  assert_selector 'div.notification.is-danger', text: 'Unable to submit place.'
+  assert_selector 'p.help.is-danger', text: 'A name is needed.'
+  assert_selector 'p.help.is-danger', text: 'You need to provide an address or location.'
+end
+
+
+# Update
+# valid fields
+Given(/^I am signed in and I try to edit a place that has been submitted$/) do
+  user  = create :user
+  place = create :place, user: user
+
+  sign_in_as user
+
+  visit "/places/#{place.to_param}/edit"
+end
+
+When(/^I fill in the name field for that place$/) do
+  fill_in 'place[name]', with: 'Aliya Surf Resort'
+end
+
+Then(/^I should be on the place page with an update success message$/) do
+  assert_selector 'h1.title', text: 'Aliya Surf Resort'
+  assert_selector 'div.notification.is-success', text: 'Update success!'
+end
+
+# invalid fields
+When(/^I leave the name and address fields blank for that place$/) do
+  fill_in 'place[name]', with: ''
+  fill_in 'place[address]', with: ''
+end
+
+Then(/^I should still be on the place edit page with error messages for name and address$/) do
+  assert_selector 'div.notification.is-danger', text: 'Unable to update place.'
+  assert_selector 'p.help.is-danger', text: 'A name is needed.'
+  assert_selector 'p.help.is-danger', text: 'You need to provide an address or location.'
+end
+
+# Deleting a place
+Given(/^I am signed in and I go to a place's page$/) do
+  user  = create :user
+  place = create :place, user: user
+
+  sign_in_as user
+
+  visit "/places/#{place.to_param}"
+end
+
+When(/^I click on the delete "trash can" button$/) do
+  accept_alert do
+    find('i.fas.fa-trash').click
+  end
+end
+
+Then(/^I should be redirected to the home page with a successful delete message$/) do
+  assert_selector 'div.notification.is-success', text: 'Place deleted successfuly.'
+  assert_selector '.notification.is-info', text: 'There are no places yet.'
+end
