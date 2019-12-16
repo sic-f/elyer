557171279d33c8d1831abc9aa621ef6eacc37590
Francis Pineda <kikopineda@gmail.com>
2019-12-16 17:58:23 +0800

    :sparkles: Add User avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ class UsersController < Clearance::UsersController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :avatar)
   end
 end
