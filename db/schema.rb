80a76b900e5ddeb046e3703cc2ffdaeb746f576b
Francis Pineda <kikopineda@gmail.com>
2019-12-18 23:17:09 +0800

    :wrench::art: Create and run migration for Image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_204541) do
+ActiveRecord::Schema.define(version: 2019_12_18_151413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2019_12_14_204541) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.text "caption"
+    t.string "type"
+    t.bigint "place_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_images_on_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -67,5 +76,6 @@ ActiveRecord::Schema.define(version: 2019_12_14_204541) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "places"
   add_foreign_key "places", "users"
 end
