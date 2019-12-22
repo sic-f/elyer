11f8f41b7c6e1ff8fec9d4125862db6dba7b4190
Francis Pineda <kikopineda@gmail.com>
2019-12-22 23:32:40 +0800

    :lipstick: Fix UI design and layout of place and images pages

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,9 @@
 module ImagesHelper
-  def images_authorized_to(operation, resource)
+  def image_authorized_to(operation, resource)
     ImagePolicy.new(current_user, resource).send(operation)
+  end
+
+  def photo_edit_caption_label(photo)
+    photo.caption? ? 'Edit description' : 'Add description'
   end
 end
