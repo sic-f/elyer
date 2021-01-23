e1e45dd236148a5588a19c6a0584c4ca65245e2e
Francis Pineda <frpine@outlook.com>
2021-01-24 00:27:12 +0800

    ✨ Prevent comments when not authenticated

---
+++ spec/requests/comments/images/comments_authorization_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Comments authorization', type: :request do
+  context 'when not authenticated' do
+    let(:image) { build_stubbed :photo }
+    let(:params) do
+      { comment: {
+        content: 'comment',
+        commentable_id: image.id,
+        commentable_type: 'Image' }
+      }
+    end
+
+    it 'redirects to login page' do
+      post "/images/#{image.to_param}/comments", params: params
+
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to '/login'
+    end
+  end
+end
