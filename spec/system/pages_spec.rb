a1ab0a5dd5b33a7da306e6fe113be65374ebf10c
Francis Pineda <frpine@outlook.com>
2021-02-06 16:48:09 +0800

    ✅ Convert pages tests to system specs

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'Pages', type: :system do
-  it 'index page has no places' do
+feature 'Pages' do
+  scenario 'index page has no places' do
     visit '/'
 
     expect(page).to have_selector '.notification', text: 'There are no places yet.'
   end
 
-  it 'index page has places' do
+  scenario 'index page has places' do
     create :place
 
     visit '/'
