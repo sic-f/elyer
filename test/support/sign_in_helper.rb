73e35878dee1758e84fa9217bb095b67aedcbf2c
Francis Pineda <sic-f@users.noreply.github.com>
2021-02-07 18:08:36 +0800

    Fix place submission bug (#80) [Finishes #176835935]
    * ✅ Remove unnecessary test
    
    * 🐛 Fix submission of places witout extra photos
    
    * 🎨 Fix alignment of local variables
    
    * 👬 Add clearance backdoor helpers for integration tests

--- test/support/sign_in_helper.rb
+++ test/support/sign_in_helper.rb
@@ -8,4 +8,15 @@ module SignInHelper
   def sign_in_as(user)
     visit root_path as: user
   end
+
+  module IntegrationTests
+    def sign_in
+      @user = create :user
+      get root_path as: @user
+    end
+
+    def sign_in_as(user)
+      get root_path as: user
+    end
+  end
 end
