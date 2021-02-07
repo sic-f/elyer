56a860a04a5ca8cbc51461f7a1f19584bf873b65
Francis Pineda <frpine@outlook.com>
2021-02-07 17:22:27 +0800

    ✅ Remove unnecessary test

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,18 +1,6 @@
 require 'application_system_test_case'
 
 class PagesTest < ApplicationSystemTestCase
-  test 'navigation bar' do
-    visit '/'
-
-    assert_selector 'nav.navbar', text: 'Balai'
-  end
-
-  test 'side navigation' do
-    visit '/'
-
-    assert_selector 'aside.menu', text: 'HOME'
-  end
-
   test 'index page has no places' do
     visit '/'
 
